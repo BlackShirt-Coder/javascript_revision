55c0f676dae9b089f7f8a2193ad2d0b3b0281019
BlackShirt-Coder <haruta685@gmail.com>
2023-04-13 22:03:07 +0630

    json format

---
+++ Lesson_2/7_json_data_format.js
@@ -0,0 +1,7 @@
+let user= {
+    'name': 'john',
+    'age': 20
+};
+// console.log(JSON.stringify(user));
+let jsonData=JSON.stringify(user);//Json data string
+console.log(JSON.parse(jsonData));//javascript object
